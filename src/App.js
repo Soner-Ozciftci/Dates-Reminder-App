2e1b39a1dcc59d5eec7cb4a015db4eded1bfefe5
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-09 17:08:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React ,{useState} from 'react';
+import './App.css';
+import data from './Data';
+
+
+function App() {
+  return (
+  <main>
+    <section className='container'>
+      <h3>National Holidays Dates</h3>
+      <button>Clear All</button>
+    </section>
+  </main>
+
+  );
+}
+
+export default App;
