e14cdfbb03b1023c4fff7d24f45f496636a9fc85
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-09 17:16:11 +0200

    holiday dates add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React ,{useState} from 'react';
-import './App.css';
-import data from './Data';
-
+import React, { useState } from "react";
+import "./App.css";
+import data from "./Data";
 
 function App() {
-  return (
-  <main>
-    <section className='container'>
-      <h3>National Holidays Dates</h3>
-      <button>Clear All</button>
-    </section>
-  </main>
+  const [holidays, setHolidays] = useState(data);
 
+  return (
+    <main>
+      <section className="container">
+        <h3>National Holidays Dates</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div className="holiday" key={holiday.id}>
+              <img src={holiday.img} />
+              <div>
+                <p>{holiday.title}</p>
+                <p>{holiday.date}</p>
+              </div>
+            </div>
+          );
+        })}
+        <button>Clear All</button>
+      </section>
+    </main>
   );
 }
 
